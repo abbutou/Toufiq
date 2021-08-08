aa15cdc8785a048e62eaaac2179b740c588fde6e
abbutou <88619031+abbutou@users.noreply.github.com>
2021-08-08 20:04:18 +0600

    Toufiq.py
    apt update 
    apt upgrade
    pkg install git
    pkg install python
    pkg install python2
    pip2 install requestes
    pip2 install mechanize
    git clone https://github.com/abbutou/Toufiq.git
    cd Toufiq.git
    ls
    ./Toufiq.sh

---
+++ Toufiq.py
@@ -0,0 +1,172 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import os
+import shutil
+import sys
+import subprocess
+import string
+import random
+import json
+import re
+import time
+import argparse
+import zipfile
+from io import BytesIO
+
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+from utils.decorators import MessageDecorator
+from utils.provider import APIProvider
+
+try:
+    import requests
+    from colorama import Fore, Style
+except ImportError:
+    print("\tSome dependencies could not be imported (possibly not installed)")
+    print(
+        "Type `pip3 install -r requirements.txt` to "
+        " install all required packages")
+    sys.exit(1)
+
+
+def readisdc():
+    with open("isdcodes.json") as file:
+        isdcodes = json.load(file)
+    return isdcodes
+
+
+def get_version():
+    try:
+        return open(".version", "r").read().strip()
+    except Exception:
+        return '1.0'
+
+
+def clr():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+
+def bann_text():
+    clr()
+    logo = """
+   Team Sk Brother's 
+   All Spammer Vai Brother's
+   
+                                         """
+    if ASCII_MODE:
+        logo = ""
+    version = "Version: "+__VERSION__
+    contributors = "Contributors: "+" ".join(__CONTRIBUTORS__)
+    print(random.choice(ALL_COLORS) + logo + RESET_ALL)
+    mesgdcrt.SuccessMessage(version)
+    mesgdcrt.SectionMessage(contributors)
+    print()
+
+
+def check_intr():
+    try:
+        requests.get("https://motherfuckingwebsite.com")
+    except Exception:
+        bann_text()
+        mesgdcrt.FailureMessage("Poor internet connection detected")
+        sys.exit(2)
+
+
+def format_phone(num):
+    num = [n for n in num if n in string.digits]
+    return ''.join(num).strip()
+
+
+def do_zip_update():
+    success = False
+    if DEBUG_MODE:
+        dir_name = "SkBomb-dev"
+    else:
+     https://www.facebook.com/groups/121843966710069/?ref=share"
+        dir_name = "SkBomb-master"
+    print(ALL_COLORS[0]+"Downloading ZIP ... "+RESET_ALL)
+    response = requests.get(zip_url)
+    if response.status_code == 200:
+        zip_content = response.content
+        try:
+            with zipfile.ZipFile(BytesIO(zip_content)) as zip_file:
+                for member in zip_file.namelist():
+                    filename = os.path.split(member)
+                    if not filename[1]:
+                        continue
+                    new_filename = os.path.join(
+                        filename[0].replace(dir_name, "."),
+                        filename[1])
+                    source = zip_file.open(member)
+                    target = open(new_filename, "wb")
+                    with source, target:
+                        shutil.copyfileobj(source, target)
+            success = True
+        except Exception:
+            mesgdcrt.FailureMessage("Error occured while extracting !!")
+    if success:
+        mesgdcrt.SuccessMessage("SkBomb was updated to the latest version")
+        mesgdcrt.GeneralMessage(
+            "Please run the script again to load the latest version")
+    else:
+        mesgdcrt.FailureMessage("Unable to update SkBomb.")
+        mesgdcrt.WarningMessage(
+            "Grab The Latest one From https://www.facebook.com/groups/121843966710069/?ref=share")
+
+    sys.exit()
+
+def do_git_update():
+    success = False
+    try:
+        print(ALL_COLORS[0]+"UPDATING "+RESET_ALL, end='')
+        process = subprocess.Popen("git checkout . && git pull ",
+                                   shell=True,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.STDOUT)
+        while process:
+            print(ALL_COLORS[0]+'.'+RESET_ALL, end='')
+            time.sleep(1)
+            returncode = process.poll()
+            if returncode is not None:
+                break
+        success = not process.returncode
+    except Exception:
+        success = False
+    print("\n")
+
+    if success:
+        mesgdcrt.SuccessMessage("SkBomb was updated to the latest version")
+        mesgdcrt.GeneralMessage(
+            "Please run the script again to load the latest version")
+    else:
+        mesgdcrt.FailureMessage("Unable to update TBomb.")
+        mesgdcrt.WarningMessage("Make Sure To Install 'git' ")
+        mesgdcrt.GeneralMessage("Then run command:")
+        print(
+            "git checkout . && "
+            "https://www.facebook.com/groups/121843966710069/?ref=share")
+    sys.exit()
+
+def update():
+    if shutil.which('git'):
+        do_git_update()
+    else:
+        do_zip_update()
+
+
+def check_for_updates():
+    if DEBUG_MODE:
+        mesgdcrt.WarningMessage(
+            "DEBUG MODE Enabled! Auto-Update check is disabled.")
+        return
+    mesgdcrt.SectionMessage("Checking for updates")
+    fver = requests.get(
+        "https://raw.githubusercontent.com/TheSpeedX/TBomb/master/.version"
+    ).text.strip()
+    if fver != __VERSION__:
+        mesgdcrt.WarningMessage("An update is available")
+        mesgdcrt.GeneralMessage("Starting update..."
